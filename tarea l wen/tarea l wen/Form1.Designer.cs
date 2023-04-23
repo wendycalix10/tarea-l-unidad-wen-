62136ccc06e672254dfe3fbc4822aa729a3d4496
wendycalix10 <124416967+wendycalix10@users.noreply.github.com>
2023-04-23 09:21:44 -0600

    tarea #1
    este es mi primer ejercicio de tarea.

---
+++ tarea l wen/tarea l wen/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace tarea_l_wen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1ingresarnumero = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Agency FB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Teal;
+            this.button1.Location = new System.Drawing.Point(133, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 54);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "RESULTADO ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox2.Location = new System.Drawing.Point(118, 130);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(268, 57);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(485, 222);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 7;
+            // 
+            // label1ingresarnumero
+            // 
+            this.label1ingresarnumero.AutoSize = true;
+            this.label1ingresarnumero.Font = new System.Drawing.Font("Agency FB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1ingresarnumero.ForeColor = System.Drawing.Color.Teal;
+            this.label1ingresarnumero.Location = new System.Drawing.Point(71, 57);
+            this.label1ingresarnumero.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1ingresarnumero.Name = "label1ingresarnumero";
+            this.label1ingresarnumero.Size = new System.Drawing.Size(187, 28);
+            this.label1ingresarnumero.TabIndex = 6;
+            this.label1ingresarnumero.Text = "Ingrese numero entero";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(477, 309);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1ingresarnumero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1ingresarnumero;
+    }
+}
+
---
+++ tarea l wen/tarea l wen/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tarea_l_wen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero;
+            numero = Convert.ToInt32(textBox1.Text);
+
+            int result = numero / 2;
+
+            if (result == 0)
+            {
+                textBox2.Text = "El numero ingresado es par y positivo";
+            }
+
+            else
+            {
+                textBox2.Text = "El numero ingresado es impar y positivo";
+            }
+
+            if (result > 0)
+            {
+                textBox2.Text = "El numero ingresado es positivo";
+
+            }
+            else
+            {
+                textBox2.Text = "El numero ingresado es negativo";
+            }
+        }
+    }
+}
